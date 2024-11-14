e4b51c048380e8fd7f9ccb6c96bac73e25efc767
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-11-15 00:00:10 +0500

    tyuiu-fastapi-rec-sys 14.11.2024 0.0.1.6

--- src/config.py
+++ src/config.py
@@ -23,7 +23,7 @@ class StandardScalerSettings(BaseSettings):
 class AppSettings(BaseSettings):
     host: str = "0.0.0.0"
     port: int = 8000
-    workers: int = 3
+    workers: int = 1
     timeout: int = 900
 
 
