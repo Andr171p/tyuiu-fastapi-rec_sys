d04fecb78b094e35e5b145da5cd6eee78a479f56
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-11-28 01:41:47 +0500

    tyuiu-fastapi-rec-sys 28.11.2024 0.0.2.12

--- main.py
+++ main.py
@@ -58,9 +58,11 @@ app.include_router(
 )
 
 app.add_middleware(GlobalMiddleware)
+origins = ["http://localhost:3000", "http://127.0.0.1:3000"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
